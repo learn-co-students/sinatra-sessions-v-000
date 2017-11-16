a8683a714f0fab56d52fb92e4a916d33327f819a
Julianne Kim <juliannne@gmail.com>
2017-11-16 03:01:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -54,6 +54,9 @@ class App < Sinatra::Base
   get '/logout' do
     #clear session hash here
     session.clear
+
+    # OR
+    # session = {}
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
