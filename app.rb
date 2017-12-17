80a21c86a8d08ad5d2119e74c46483997bde0fd8
chrissyhunt87 <chrissy.hunt@gmail.com>
2017-12-17 11:08:56 -0500

    Done.

--- app.rb
+++ app.rb
@@ -19,11 +19,10 @@ class App < Sinatra::Base
   end
 
   get '/set' do
-    # set the :foo key of the session hash equal to 'hello' here!
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
-      "Session value has not been set!"
+      session[:foo] = 'hello'
     end
   end
 
@@ -36,13 +35,12 @@ class App < Sinatra::Base
   end
 
   get '/set_session' do
-    #set session id here
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
       redirect '/fetch_session_id'
     else
-      "Session ID has not been set!"
+      session[:id] = 1
     end
   end
 
@@ -52,6 +50,7 @@ class App < Sinatra::Base
 
   get '/logout' do
     #clear session hash here
+    session.clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
