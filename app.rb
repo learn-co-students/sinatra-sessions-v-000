cc5c284f510c79084fd80f4e54e547b88abe3e52
Phoebe Lau <phoebelau7@gmail.com>
2018-01-12 03:32:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -15,11 +15,14 @@ class App < Sinatra::Base
   end
 
   get '/first_exercise' do
-    "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, naviate to the '/set' path."
+    "Your first exercise will be to set your session key-value pair.\n
+    In the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\n
+    Then, naviate to the '/set' path."
   end
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
+    session[:foo] = 'hello'
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
@@ -32,12 +35,14 @@ class App < Sinatra::Base
   end
 
   get '/second_exercise' do
-    "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
+    "Your second lesson will be to set your session :id key to a value.\n
+    In the route: /set_session, write a line of code that sets the session[:id] equal to 1.\n
+    Then, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
     #set session id here
-
+    session[:id] = 1
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
       redirect '/fetch_session_id'
@@ -51,6 +56,7 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
+    session.clear
     #clear session hash here
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
