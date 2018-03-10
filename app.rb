8b0409b6e110fdfae33161c084bde93ca0b46c2c
Obafemi Fadairo <ofpupp@princeton.edu>
2018-03-10 00:36:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -51,7 +51,7 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
-    session.delete(:id)
+    session.delete_if {|key, value| 1 == 1}
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
