346d8558fd77f96ab0fc52d80ca108f0af51b458
Blake A. Long <blake.a.long@gmail.com>
2020-01-28 14:07:30 -0700

    Done.

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'thin'
 gem 'shotgun'
 gem 'pry'
 gem 'require_all'
+gem 'sysrandom'
 
 group :test do
   gem 'rspec'
--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,6 @@
 ENV['SINATRA_ENV'] ||= "development"
+require 'sysrandom/securerandom'
+
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
